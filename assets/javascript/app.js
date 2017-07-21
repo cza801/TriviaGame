9e51077624d15718776fd0ba2bac80bc0696ab0d
Cesar <cesssar626@gmail.com>
2017-07-20 18:33:01 -0600

    Added more JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,59 @@
-var timer = 30;
-
+var timer = 30; //
+var correctAns;
+var wrongAns;
 var intervalId;
 
+
+//-------------------------------------Starting the game--------------------
 $(document).ready(function(){
 
-	$("#countdown").html(timer);
-
-	function run() {
+	//here we are hiding the trivia questions until the start button is pressed. 
+	$("#triviapage").hide();
+	$("#scorepage").hide();
+	// Here, i am adding an on click function to hide the panel with the "start" button, and displaying the trivia questions
+	$(".btn").on("click", function () {
+		$("#startpage").hide ();
+		$("#triviapage").show ();
+		timer = 30;
 		intervalId = setInterval(decrement, 1000);
+	});
+	
+	//This will store the correct, or incorrect answer, on the click of the radio button. 
+	$("#response")on("click", function() { 
+		if () ; 
 
-	};
-
-	function decrement() {
-
-		timer--;
-
-		$("#countdown").html(timer);
-
-		if (timer === 0) {
-
-			alert("Stop! drop! shut 'em down, open up shop!");
-
-			clearInterval(intervalId);
-		}
-	}
+	});
 
 
-run();
+	//This is the timer, and once the timer gets to 0, an alert will pop up, and stop the timer. 
+	
+
+		function decrement() {
+
+			timer--;
+
+			$("#countdown").html(timer);
+
+			if (timer === 0) {
+
+				$("#triviapage").hide();
+
+				alert("Stop, drop, shut 'em down, open up shop!");
+
+				clearInterval(intervalId);
+
+				$("#scorepage").show();
+				
+				$("#startpage").show ();
+			}
+		};
+
+
+
+//This shows the current time left. 	
+$("#countdown").html(timer);
+
+
 decrement ();
 
 
@@ -39,5 +66,4 @@ decrement ();
 
 
 
-
-	});
+});
